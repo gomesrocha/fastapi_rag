49d799e5fb03839315fcfeebdf43729dc03e36fb
root <gomesrocha@gmail.com>
2024-07-17 20:15:45 +0000

    update project

--- api_tests.sh
+++ api_tests.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-echo "Testing the root endpoint..."
-curl http://127.0.0.1:8000/
-
-
+# Testing file upload
 echo "Testing file upload..."
-curl -X 'POST' 'http://127.0.0.1:8000/uploadfile/' -F 'file=@/usercode/obama.txt'
-
-
-echo "Testing the ask question endpoint..."
 curl -X 'POST' \
-'http://127.0.0.1:8000/ask/' \
+'http://localhost:8000/uploadfile/' \
+-H 'accept: application/json' \
+-H 'Content-Type: multipart/form-data' \
+-F 'file=@/usercode/obama.txt;type=text/plain'
+echo ""
+echo ""
+
+# Testing the root endpoint
+echo "Testing the root endpoint..."
+curl -X 'GET' 'http://localhost:8000/' -H 'accept: application/json'
+echo ""
+echo ""
+
+# Wait for 30 seconds before testing question answering
+echo "Waiting for 30 seconds before testing question answering..."
+sleep 30
+
+# Testing question answering
+echo "Testing question answering..."
+curl -X 'POST' \
+'http://localhost:8000/ask/' \
+-H 'accept: application/json' \
 -H 'Content-Type: application/json' \
--d '{"question": "What is the capital of France?"}'
-Nota: Substitua pelo caminho para o arquivo que você deseja testar pelo ponto de extremidade de carregamento./usercode/filename.txt
+-d '{"document_id": 1, "question": "When was Barack Obama elected for the second time ?"}'
+echo ""
+echo ""
+
+# Testing similar chunk retrieval
+echo "Testing similar chunk retrieval..."
+curl -X 'POST' \
+'http://localhost:8000/find-similar-chunks/1' \
+-H 'accept: application/json' \
+-H 'Content-Type: application/json' \
+-d '{"question": "Who is Barack Obama wife and when did they meet ?"}'
+echo ""
+echo ""
